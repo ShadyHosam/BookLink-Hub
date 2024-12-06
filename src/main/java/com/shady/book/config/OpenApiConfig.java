481e40f1eff6d3f1f3027e7b9b0e1ef53c215ab4
shady <shadyhosam200@gmail.com>
2024-12-06 22:56:45 +0200

    fixed secuirty config for Swagger

--- src/main/java/com/shady/book/config/OpenApiConfig.java
+++ src/main/java/com/shady/book/config/OpenApiConfig.java
@@ -22,7 +22,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         ),
         servers = {
                 @Server(
-                        description = "Local Server",
+                        description = "Local ENV",
                         url = "http://localhost:8088/api/v1"
                 ),
                 @Server(
